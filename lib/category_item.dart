3eb66be9b4458787c6db817b27ae7a416cce0748
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-27 04:31:06 +0100

    passed category screen data via constructors

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
-  const CategoryItem(
+
+  CategoryItem(
+    this.id,
     this.title,
     this.color,
   );
@@ -12,7 +15,7 @@ class CategoryItem extends StatelessWidget {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return const CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
     );
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+  // const CategoryMealsScreen({Key? key}) : super(key: key);
+
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('The Recipes'),
+        title: Text(categoryId),
       ),
-      body: const Center(
-        child: Text('The Recipe for the category!'),
+      body: Center(
+        child: Text(categoryTitle),
       ),
     );
   }
