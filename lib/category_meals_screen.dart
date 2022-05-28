98500688e09e32228459c7303ae8f0b8d3279b96
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-28 07:18:58 +0100

    Added Data model and data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -17,9 +17,11 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(categoryId),
-      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index){
+
+      },
+      itemCount: ,),
     );
   }
 }
