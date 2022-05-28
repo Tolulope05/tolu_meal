07fa7d961a02a2afb7d9e4dc72682f2e6a783ca0
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-28 03:18:21 +0100

    Using Static constant for named routes

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: const CategoriesScreen(),
+      // home: const CategoriesScreen(),
+      initialRoute: '/', // default is '/'
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/': (ctx) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
   }
